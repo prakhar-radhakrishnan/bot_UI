77205c7dcceb78586a13ea6615eaa6dee7da92cf
prakhar-radhakrishnan <96592788+prakhar-radhakrishnan@users.noreply.github.com>
2024-08-28 12:03:57 +0530

    Update App.jsx
    Updated Case of botUI to BotUI.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import BotUI from './components/botUI'
+import BotUI from './components/BotUI'
 
 function App() {
   const [count, setCount] = useState(0)
