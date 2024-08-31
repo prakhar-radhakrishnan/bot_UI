a019100fea719a18fd5bad47212bba4057b49909
prakhar-radhakrishnan <96592788+prakhar-radhakrishnan@users.noreply.github.com>
2024-09-01 03:00:09 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,21 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-
+ const express = require('express');
 const app = express();
-
-// Middleware
-app.use(cors()); // To allow requests from your React app
-app.use(express.json()); // To parse JSON bodies
-
-// Echo endpoint
-app.post('/echo', (req, res) => {
-  const userMessage = req.body.message;
-  // Respond back with the user's message
-  res.json({ response: `You said: ${userMessage}` });
+app.use("/", (req,res)=>{
+  res.send("Servre is running")
 });
-
-// Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen (5000, console.log("Server started on port 5000"));
